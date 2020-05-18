06c9cbecc0195044b5f133ac36bd75dbb79e6220
Valerie Hill <itsvhill@gmail.com>
2020-05-17 20:29:52 -0500

    added html

--- app.js
+++ app.js
@@ -10,150 +10,208 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter your name as the manager of this team:"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your ID number?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your email address?"
-        },
-        {
-            type: "input",
-            name:  "officeNumber",
-            message: "What is your office number?"
-        },
-        {
-            type: "confirm",
-            name: "employeeNum",
-            message: "Do you want to add an employee?"  
-        }
-]).then(data => {
-    console.log(data);
-    if (data.employeeNum === true) {
-        console.log("Adding a new employee!")
-        this.promptRole();
-    }else {
-        this.quit();
-        console.log("Thank you. No more employees to add.")
-    }
-
-});
-}
-promptUser(); {
-
-    promptRole(); {
-        return inquirer.prompt([
-            {
-                type: "checkbox",
-                name: "empRole",
-                message: "What is the employee's role?",
-                choices: ["Engineer", "Intern"]
-            }
-        ]).then(val => {
-            if (val.choice === "Engineer") {
-                this.promptEngineer();
-            } else {
-                this.promptIntern();
-            }
-        });
-    }
-}
-
-promptEngineer(); {
-    inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the engineer's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their ID number?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email address?"
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "What is their github username?"
-        }
+  return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter your name as the manager of this team:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+      },
+      {
+        type: "confirm",
+        name: "employeeNum",
+        message: "Do you want to add an employee?",
+      }
     ]);
 }
 
-promptIntern(); {
-    inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the intern's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their ID number?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email address?"
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "What what school did the the intern attend?"
-        }
-    ]);
-}
-    //loops through the number of employees the manager has
+//Attempt to loop through the number of employees the manager has
 // const numberEmployees = data.employeeNum
 //     for (var i = 0; i < numberEmployees.length; i++); {
 //     console.log(`${numberEmployees[i]}`);
 // }
 
-/*
-//if Engineer then ask these questions, if Intern ask these
-const roleQuestions = JSON.parse(data);
+promptUser() 
+    .then((data) => {
+      console.log(data);
+      if (data.employeeNum === true) {
+        console.log("Adding a new employee!");
+        this.promptRole();
+      } else {
+        this.quit();
+        console.log("Thank you. No more employees to add.");
+      }
+    });
 
-roleQuestions.forEach(function(role) {
-    if (role.choices === "Engineer") {
-      dogs.push(animal);
-    } else if (role.choices === "Intern") {
-      cats.push(animal);
-    }
-  });
+promptRole()
+    .then(function(data) {
+        if (data.employeeNum === true) {
+            return inquirer.prompt([
+                {
+                type: "checkbox",
+                name: "empRole",
+                message: "What is the employee's role?",
+                choices: ["Engineer", "Intern"],
+                },
+            ])
+            .then((data) => {
+                if (data.empRole === "Engineer") {
+                this.promptEngineer();
+                } else {
+                this.promptIntern();
+                }
+            });
+        }
+    });
+    promptEngineer() 
+        return inquirer.prompt([
+        {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "What is their ID number?"
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "What is their email address?"
+        },
+        {
+        type: "input",
+        name: "github",
+        message: "What is their github username?"
+        }]);
 
-  // Turn the arrays into JSON strings so they can be written to files
-  const dogJSON = JSON.stringify(dogs, null, 2);
+    promptIntern()
+        return inquirer.prompt([
+        {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "What is their ID number?"
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "What is their email address?"
+        },
+        {
+        type: "input",
+        name: "school",
+        message: "What what school did the the intern attend?"
+        }
+    ]);
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-//function generateHTML(answers) {
-//    return `
-//    copy in html from other pages???? and enter ${valuepaths????}`;
-//}
+function render(data) {
+   return `
+   <!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+
+                        <div class="row">
+                            <div class="team-area col-12 d-flex justify-content-center">
+                            <div class="card employee-card">
+                            <div class="card-header">
+                                <h2 class="card-title">${data.name}</h2>
+                                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>{{ role }}</h3>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item">ID: ${data.id}</li>
+                                    <li class="list-group-item">Email: ${data.email}<a href="mailto:{{ email }}">{{ email }}</a></li>
+                                    <li class="list-group-item">Office number: ${data.officeNumber}</li>
+                                </ul>
+                            </div>
+                        </div>`
+    if (data.empRole === "Engineer") {
+        return 
+                `<div class="card employee-card">
+                        <div class="card-header">
+                            <h2 class="card-title">${data.name}</h2>
+                            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{{ role }}</h3>
+                        </div>
+                <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${data.id}</li>
+                            <li class="list-group-item">Email: ${data.email}<a href="mailto:{{ email }}">{{ email }}</a></li>
+                            <li class="list-group-item">GitHub: ${data.github}<a href="https://github.com/{{ github }}" target="_blank" rel="noopener noreferrer">{{ github }}</a></li>
+                        </ul>
+                    </div>
+                </div>`
+    }else {
+        return 
+                `<div class="card employee-card">
+                    <div class="card-header">
+                        <h2 class="card-title">${data.name}</h2>
+                        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>{{ role }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${data.id}</li>
+                            <li class="list-group-item">Email: ${data.email}<a href="mailto:{{ email }}">{{ email }}</a></li>
+                            <li class="list-group-item">School: ${data.school}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+   `;
+}
+}
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
@@ -181,4 +239,3 @@ roleQuestions.forEach(function(role) {
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
-*/
